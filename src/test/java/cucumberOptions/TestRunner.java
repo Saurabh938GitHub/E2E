faba7ee50d999bf2c6da3eba85e7b684442fa0cf
Saurabh938569 <938saurav@gmail.com>
2021-06-20 11:12:58 +0530

    commit

--- src/test/java/cucumberOptions/TestRunner.java
+++ src/test/java/cucumberOptions/TestRunner.java
@@ -6,6 +6,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
 
+
 //@RunWith(Cucumber.class)
 @CucumberOptions(
 	features= "src/test/java/features",
